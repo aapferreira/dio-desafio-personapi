ebd80537a2af62251de24080455e5e5aee123803
aapferreira <augustoprofessor@gmail.com>
2022-07-11 21:14:44 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Desenvolvendo um sistema de gerenciamento de pessoas em API REST com Spring 
+
+### Julho/2022
+DIO - Resolução do desafio
+- Seguindo as instruções do professor, copiando, colando, testando e vendo resultados.
+- Spring Boot DevTools, Lombok, Actuator, Spring Data JPA, Spring Web, H2
+- Camadas: Controller, Service, Model, Reporitory, DTO
---
+++ src/main/java/br/com/dio/personapi/DioDesafioPersonapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDesafioPersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDesafioPersonapiApplication.class, args);
+	}
+
+}
